117d50c29a3ce414b075ba808f628a46f1b59968
ana-manea <ana_manea@hotmail.com>
2025-11-16 14:24:16 -0300

    Visualizar Artista em php

--- assets/view/dashboardArtista.php
+++ assets/view/dashboardArtista.php
@@ -142,7 +142,12 @@ $pageTitle = 'Dashboard';
                     <img src="https://image-cdn-ak.spotifycdn.com/image/ab6761860000101685ec2d2af58d2b838a744ac4" alt="AnaVitoria">
                 </section>
                 <section class="hero-info">
-                    <span class="badge-verified"><?php echo translateText('ARTISTA VERIFICADO'); ?></span>
+                    <span class="badge-verified">
+                            <svg viewBox="0 0 24 24" fill="#4cb3ff" style="width:20px;">
+                                <path d="M10.814.5a1.66 1.66 0 0 1 2.372 0l2.512 2.572 3.595-.043a1.66 1.66 0 0 1 1.678 1.678l-.043 3.595 2.572 2.512c.667.65.667 1.722 0 2.372l-2.572 2.512.043 3.595a1.66 1.66 0 0 1-1.678 1.678l-3.595-.043-2.512 2.572a1.66 1.66 0 0 1-2.372 0l-2.512-2.572-3.595.043a1.66 1.66 0 0 1-1.678-1.678l.043-3.595L.5 13.186a1.66 1.66 0 0 1 0-2.372l2.572-2.512-.043-3.595a1.66 1.66 0 0 1 1.678-1.678l3.595.043zm6.584 9.12a1 1 0 0 0-1.414-1.413l-6.011 6.01-1.894-1.893a1 1 0 0 0-1.414 1.414l3.308 3.308z"/>
+                            </svg>
+                            <?php echo translateText('Artista verificado'); ?>
+                        </span>
                     <h1><?php echo translateText('AnaVitória'); ?></h1>
                     <p><?php echo translateText('21.201 ouvintes mensais'); ?></p>
                 </section>
